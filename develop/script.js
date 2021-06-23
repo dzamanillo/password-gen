ad2dfe06ac3a6162ada66bbb4d5e96deccd5b304
Daniel Zamanillo <dzamanillok@gmail.com>
2021-06-23 14:03:22 -0500

    upper case prompt added. still need arrays

--- develop/script.js
+++ develop/script.js
@@ -1,23 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
 
-  var passwordText = document.querySelector("#password");
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-}
+//   passwordText.value = password;
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+// Default Settings
+var lowerCasePrompt = true;
+var upperCasePrompt = true;
 
 // Lower Case Function
-var lowerCase = function () {
-  var lowerCasePrompt = window.confirm(
+function lowerCase() {
+  lowerCasePrompt = window.confirm(
     "Would you like to include lower case characters in your password?"
   );
-};
+}
 
-generateBtn.addEventListener("click", lowerCase);
+// Upper Case Function
+function upperCase() {
+  upperCasePrompt = window.confirm(
+    "Would you like to include upper case characters in your password?"
+  );
+}
+
+//Prompts Function for Button
+function prompts() {
+  lowerCase();
+  console.log(lowerCasePrompt);
+  upperCase();
+  console.log(upperCasePrompt);
+}
+
+generateBtn.addEventListener("click", prompts);
